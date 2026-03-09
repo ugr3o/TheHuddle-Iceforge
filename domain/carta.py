4b9819b4b0b0749965c71a7ea09b43b715596c18
hugofrancoo <hugopereirafranco47@gmail.com>
2026-03-09 15:01:10 -0300

    logica de asignacion de palo y valor a cartas

--- domain/carta.py
+++ domain/carta.py
@@ -1,9 +1,18 @@
-class cartas:
-    PALOS = ["♠️", "♥️", "♦️", "♣️"]
-    VALORES = []
-    def __init__(self,palo,valor):
+class Carta:
+    PALOS = ["♠️", "♥️", "♦️", "♣️"] #palos posibles para cada valor
+    VALORES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] # valores de las cartas como str
+    def __init__(self,palo,valor): # se utiliza el __init__ por convencion y el self para indicar que le pertenece
         self.palo = palo
         self.valor = valor
 
+    def obtener_puntos(self):
+        if self.valor in ["J","Q","K"]: # estos valores son 10
+            return 10
+        elif self.valor == "A": #
+            return 11
+        else:
+            return int(self.valor)
 
     
+    def __str__(self):
+        return f"{self.valor}{self.palo}"
